a4f068743cd666479683faf8362485aaf84f01be
Fabrizio99 <avbar357159@gmail.com>
2019-09-22 12:46:57 -0500

    done

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.txt-type');
+const texts = ['Bienvenido','Welcome','Benvenuto','Bem-vindo'];
+let indexTexts = 0;
+let indexString = 0;
+let currentText = '';
+let letter = '';
+let deleting=false;
+
+(function typeWriter(){
+    let typeSpeed = 300;
+    currentText = texts[indexTexts]
+    letter = deleting? currentText.slice(0,--indexString):currentText.slice(0,++indexString)
+    container.textContent=letter;
+    if(indexTexts===texts.length)    indexTexts=0
+    if(deleting)  typeSpeed/=2
+    if(indexString==currentText.length){
+        deleting=true
+        typeSpeed=2500
+    }else if(deleting && letter==''){
+        typeSpeed=1000;
+        deleting=false
+        indexTexts++;
+    }
+    if(indexTexts==texts.length)    indexTexts=0
+    
+    setTimeout(() => {
+        typeWriter()
+    }, typeSpeed);
+}())
